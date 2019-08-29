f0a87051142f2dec01e28e77bacbbc326dadc8be
Larry Heimann <profh@cmu.edu>
2019-08-28 21:46:07 -0400

    Some extra starting code for exercise

--- WallExercise.playground/Contents.swift
+++ WallExercise.playground/Contents.swift
@@ -1,6 +1,35 @@
-// Create a Wall class
 class Wall {
+  var bottles: Int
   
+  init(bottles: Int) {
+    
+  }
+  
+  func isEmpty() -> Bool {
+
+  }
+  
+  func singOneVerse() -> Void {
+    let line1 = sing(" on the wall, ") + sing(".\n")
+    let line2 = takeOneDown() + sing(" on the wall.\n")
+    let verse = line1 + line2
+    
+    print(verse)
+  }
+  
+  private func sing(_ extra: String = "") -> String {
+
+    
+    
+    
+  }
+  
+  private func takeOneDown() -> String {
+
+    
+    
+    
+  }
 }
 
 
